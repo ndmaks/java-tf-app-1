bcf899c65bb5276750059512741594502a99625b
ndmaks <89162690+ndmaks@users.noreply.github.com>
2023-03-05 20:42:06 +0300

    seconde version

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -5,7 +5,7 @@ variable subnet_cidr_block {
     default = "10.0.10.0/24"
 }
 variable avail_zone {
-    default = "us-east-1a"
+    default = "eu-west-1a"
 }
 variable env_prefix {
     default = "dev"
@@ -14,5 +14,5 @@ variable instance_type {
     default = "t2.micro"
 }
 variable region {
-    default = "us-east-1"
+    default = "eu-west-1"
 }
